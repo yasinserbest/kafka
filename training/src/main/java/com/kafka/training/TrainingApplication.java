356a20cacdce8a029a65aeb913c57e143935161f
yserbest <yasin.serbest@mkk.com.tr>
2025-10-28 12:19:27 +0300

    default consumer & producer

---
+++ training/src/main/java/com/kafka/training/TrainingApplication.java
@@ -0,0 +1,21 @@
+package com.kafka.training;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Properties;
+
+public class TrainingApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ training/src/main/java/com/kafka/training/entity/User.java
@@ -0,0 +1,7 @@
+package com.kafka.training.entity;
+
+public class User {
+    String name;
+    String email;
+    Integer tckn;
+}
